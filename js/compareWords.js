377d93e42f30fb9bdf2f7e8de8153fad0a7a7838
DarkMatr126 <95891701+DarkMatr126@users.noreply.github.com>
2024-03-29 13:58:59 -0600

    Update compareWords.js

--- js/compareWords.js
+++ js/compareWords.js
@@ -4,8 +4,15 @@ const compareWords = (w1,w2) => {
 
     var stan1 = Math.stanDev(...cs1);
     var stan2 = Math.stanDev(...cs2);
+    var difStan = Math.abs(stan1/12.5 - stan2/12.5)
+
+    // var mean1 = Math.mean(...cs1);
+    // var mean2 = Math.mean(...cs2);
+    // var difMean = Math.abs(mean2-mean1)/25;
     
-    return 1 - Math.abs(stan1/12.5 - stan2/12.5);
+    var dif = difStan;
+
+    return 1 - dif;
 }
 
 Math.mean = (...args) => {
